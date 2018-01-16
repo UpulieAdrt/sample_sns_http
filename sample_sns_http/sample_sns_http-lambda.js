3a1c65424d06ddc4b2c00dd72bbc00d7d2878436
UpulieAdrt <35255575+UpulieAdrt@users.noreply.github.com>
2018-01-16 14:55:38 +0530

    add another sns to publish to https

--- sample_sns_http/sample_sns_http-lambda.js
+++ sample_sns_http/sample_sns_http-lambda.js
@@ -11,11 +11,28 @@ exports.handler = function (event, context, callback) {
 		.then(data => {
 			// your code goes here
 
-			console.log("success" ,data);
+			console.log("success", data);
 		})
 		.catch(err => {
 			// error handling goes here
-			console.log("failed" ,err);
+			console.log("failed", err);
+		});
+
+	sns.publish({
+		Message: 'message -/d',
+		Subject: 'Test http',
+		MessageAttributes: {},
+		MessageStructure: 'String',
+		TopicArn: 'arn:aws:sns:us-east-1:480964559519:http_publisher'
+	}).promise()
+		.then(data => {
+			// your code goes here
+
+			console.log("success",data );
+		})
+		.catch(err => {
+			// error handling goes here
+			console.log("error" ,err);
 		});
 
 
