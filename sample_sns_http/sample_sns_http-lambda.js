9f9d7c170776f74abc80ddab6bbdfca309d2ed8d
UpulieAdrt <35255575+UpulieAdrt@users.noreply.github.com>
2018-01-16 14:11:01 +0530

    added the SNS to get attributes

--- sample_sns_http/sample_sns_http-lambda.js
+++ sample_sns_http/sample_sns_http-lambda.js
@@ -1,7 +1,23 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const sns = new AWS.SNS();
+exports.handler = function (event, context, callback) {
 
 
-	callback(null,'Successfully executed');
+	callback(null, 'Successfully executed');
 	console.log("Successfully Triggered");
+	sns.getTopicAttributes({
+		TopicArn: 'arn:aws:sns:us-east-1:480964559519:publisher_topic'
+	}).promise()
+		.then(data => {
+			// your code goes here
+
+			console.log("success" ,data)
+		})
+		.catch(err => {
+			// error handling goes here
+			console.log("failed" ,err)
+		});
+
+
+
 }
